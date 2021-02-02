c75508b5dc07c152c689950b474c7238b5838c86
phuney <nva3.vaaa@gmail.com>
2021-02-03 00:50:27 +0700

    Pembaruan Koleksi Item

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,8 +1,8 @@
-import react from "react";
+import React from "react";
 import MenuItem from "../menu-item/menu-item.component";
 import "./directory.styles.scss";
 
-class Directory extends react.Component {
+class Directory extends React.Component {
   constructor() {
     super();
 
