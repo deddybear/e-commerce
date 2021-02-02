eecffad5bc0a7e8dc5e0d992cab2bf2ce55a6cae
phuney <nva3.vaaa@gmail.com>
2021-02-02 17:09:39 +0700

    perbaikan overflow pada menu

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,10 @@
 import "./menu-item.styles.scss";
 
 const MenuItem = ({ judul, imageUrl, ukuran }) => (
-  <div
-    style={{ backgroundImage: `url(${imageUrl})` }}
-    className={`${ukuran} menu-item`}
-  >
+  <div className={`${ukuran} menu-item`}>
+    <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }}/>
     <div className="content">
-      <h1 className="title">{judul}</h1>
+      <h1 className="title">{judul.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
